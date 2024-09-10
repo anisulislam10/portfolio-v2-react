1ef108246717fcf17a857e6b57b3c3f9e63d8bf5
Anisul Islam <anis.inbox10@gmail.com>
2024-09-10 11:47:45 -0700

    minor changes

--- frontend/src/components/homepage/HeroSection.jsx
+++ frontend/src/components/homepage/HeroSection.jsx
@@ -46,10 +46,10 @@ function HeroSection() {
             src={HeroSectionPitcture} 
             alt="Header" 
             width="70%" 
-            className='md:ml-[700px] md:mt-[-1px]  xsm:ml-[330px] xsm:mt-[190px]' 
+            className='md:ml-[700px] md:mt-[-1px]  xsm:ml-[190px] xsm:mt-[230px]' 
           />
         </div>
-        <div className='md:flex md:flex-col md:items-start md:ml-10 xsm:flex xsm:flex-col xsm:items-center xsm:ml-[10px]'>
+        <div className='md:flex md:flex-col md:items-start md:ml-10 xsm:flex xsm:flex-col xsm:items-center xsm:ml-[200px]'>
           <h1 className='text-white md:text-[50px] md:font-semibold md:ml-[-480px] mt-[100px] xsm:text-[25px] xsm:ml-[-480px] '>
             HI, I am
           </h1>
@@ -59,7 +59,7 @@ function HeroSection() {
           <span className=' text-[#faece3] md:text-[30px] font-semibold mt-4 md:ml-[-470px] xsm:text-[20px]' >
              {text} <span className=' md:ml-[2px] text-orange-300 xsm:ml-[600px] '>|</span>
           </span>
-          <div className='grid grid-cols-4 gap-4 mt-10 ml-[-400px] text-white xsm:mr-[100px] xsm:ml-[-480px] xsm:mt-16'  >
+          <div className='grid grid-cols-4 gap-4 mt-10 ml-[-400px] text-white xsm:mr-[100px] xsm:ml-[-380px] xsm:mt-16'  >
           <h1 className='md:shadow-2xl'> <a href="mailto:anis.inbox10@gmail.com"> <AlternateEmailIcon className="text-white hover:text-orange-400 transition-all duration-300 ease-in-out shadow-xl bg-blue-500 rounded-md px-1"  /> </a> </h1>
           <h1><a href="https://www.github.com/anisulislam10"> <GitHubIcon className="md:text-white md:hover:text-orange-400 md:transition-all md:duration-300 md:ease-in-out shadow-xl bg-blue-500 md:rounded-md px-1" /></a> </h1>
           <h1> <a href="https://linkedin.com/in/ianisulislam"><LinkedInIcon  className="md:text-white md:hover:text-orange-400 md:transition-all md:duration-300 md:ease-in-out md:shadow-xl bg-blue-500 md:rounded-md px-1" /> </a></h1>
